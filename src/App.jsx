e76e9bf28b5b89a6679bfea2048ce993c0ef1e6b
Ermias Haimanot <ermiashtsegaye@gmail.com>
2025-10-27 14:37:18 +0300

    Add a category option

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import { useState, useEffect, Fragment } from "react";
+import { useState, Fragment } from "react";
 import axios from "axios";
 
 function App() {
   const [joke, setJoke] = useState("");
-  const apiLink = "https://v2.jokeapi.dev/joke/Any?format=txt&";
 
-  async function fetchJoke() {
-    let response = await axios.get(apiLink);
-
-    setJoke(response.data);
+  async function fetchJoke(
+    // apiLink = "https://v2.jokeapi.dev/joke/Any?format=txt"
+    cat = "Any"
+  ) {
+    try {
+      let response = await axios.get(
+        `https://v2.jokeapi.dev/joke/${cat}?format=txt`
+      );
+      setJoke(response.data);
+    } catch (error) {
+      console.error("Error fetching joke:", error);
+      setJoke("😅 Oops! Failed to fetch a joke. Try again!");
+    }
   }
 
   return (
@@ -19,9 +27,58 @@ function App() {
       </div>
       <div className="bg-green-600 flex-3 flex flex-col">
         <div className="flex justify-center m-4 flex-col items-center">
-          <button onClick={fetchJoke} className="bg-amber-300 p-2 w-fit ">
-            Joke
+          <button
+            onClick={() => fetchJoke()}
+            className="bg-amber-300 p-2 w-fit "
+          >
+            Generate a joke
           </button>
+          <div className="flex flex-col items-center">
+            <p className="">A joke by category</p>
+
+            <div className="flex gap-2">
+              <button
+                onClick={() => {
+                  fetchJoke("Programming");
+                }}
+                className="bg-amber-300 p-2 w-fit "
+              >
+                Programming
+              </button>
+              <button
+                onClick={() => {
+                  fetchJoke("Dark");
+                }}
+                className="bg-amber-300 p-2 w-fit "
+              >
+                Dark
+              </button>
+              <button
+                onClick={() => {
+                  fetchJoke("Pun");
+                }}
+                className="bg-amber-300 p-2 w-fit "
+              >
+                Pun
+              </button>
+              <button
+                onClick={() => {
+                  fetchJoke("Spooky");
+                }}
+                className="bg-amber-300 p-2 w-fit "
+              >
+                Spooky
+              </button>
+              <button
+                onClick={() => {
+                  fetchJoke("Christmas");
+                }}
+                className="bg-amber-300 p-2 w-fit "
+              >
+                Christmas
+              </button>
+            </div>
+          </div>
         </div>
 
         <div>{joke}</div>
