a619f2046258112b9dd5a0d32062321cfaa27dd9
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-08-31 17:32:52 +0300

    added nav bar and styled it

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import "./NavBar.css";
+export default function NavBar() {
+  return (
+    <nav>
+      <div className="links-wrapper">
+        <a href="">Home</a>
+        <a href="">Discover</a>
+        <a href="">About Me</a>
+      </div>
+    </nav>
+  );
+}
