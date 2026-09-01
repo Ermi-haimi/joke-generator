45f0c9a47029c8cee8f1d6d113b1870988fac99e
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-09-01 23:38:55 +0300

    added components to home page

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,9 +1,13 @@
+import Features from "../../components/features/Features";
 import Hero from "../../components/hero/Hero";
+import WhatPeopleGot from "../../components/whatPeopleGot/WhatPeopleGot";
 
 export default function HomePage() {
   return (
     <main>
       <Hero />
+      <WhatPeopleGot />
+      <Features />
     </main>
   );
 }
